a80630f918c317204c3e1ee6a91f9a072ff76d44
YoJyo <45581379+YoJyo@users.noreply.github.com>
2018-12-19 18:38:02 +0900

    Prt18-2

--- Practice/src/practice17/ptra17/PTra17_04.java
+++ Practice/src/practice17/ptra17/PTra17_04.java
@@ -15,6 +15,18 @@ public class PTra17_04 {
 		 * ★ strに入っている文字列を1文字ずつゆっくり表示させるプログラムを完成させてください
 		 * ※ Thread.sleep を使用すること
 		 */
+		String[]array=str.split("",0);
 
-	}
+		for(int i=0;i<array.length;i++) {
+
+		try{
+
+			  Thread.sleep(1000);
+
+		}catch (InterruptedException e){
+
+			}
+		 System.out.print(array[i]);
+		}
+   }
 }
