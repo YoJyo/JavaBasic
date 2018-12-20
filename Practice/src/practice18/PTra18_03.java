b5b2ccd65797189661c0e15e61991ecdc62f2949
YoJyo <45581379+YoJyo@users.noreply.github.com>
2018-12-20 16:57:30 +0900

    Prt18-4

--- Practice/src/practice18/PTra18_03.java
+++ Practice/src/practice18/PTra18_03.java
@@ -6,6 +6,13 @@
  */
 package practice18;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import entity.Player;
+
 public class PTra18_03 {
 
 	/*
@@ -19,13 +26,41 @@ public class PTra18_03 {
 		 * ★ file/BestElevenCandidate.csvの内容を取得し、１行毎にPlayerインスタンスに情報を格納してください
 		 * ★ ArrayListを作成して、Playerインスタンスを格納してください
 		 */
+		ArrayList<Player> array = new ArrayList<Player>();
+		try (Scanner scanner = new Scanner(new File("file/BestElevenCandidate.csv"))) {
+			while (scanner.hasNext()) {
+				String line = scanner.nextLine();
+				String[] array1 = line.split(",", 0);
+				Player player = new Player();
 
+				player.setPosition(array1[0]);
+				player.setName(array1[1]);
+				player.setCountry(array1[2]);
+				player.setTeam(array1[3]);
 
+				array.add(player);
+
+			}
+
+		} catch (FileNotFoundException e) {
+			System.out.println("ファイルが見つかりません");
+		}
 		// ★ ①のArrayListの中から"レアル・マドリード", "バルセロナ"の選手を除外してください
+		for (int i = 0; i < array.size(); i++) {
+			if (array.get(i).getTeam().contains("レアル・マドリード")) {
+				array.remove(i);
+			}
+			if (array.get(i).getTeam().contains("バルセロナ")) {
+				array.remove(i);
+			}
 
+		}
 
 		// ★ 削除後のArrayListの中身を全件出力してください
+		for (Player str : array) {
 
+			System.out.println(str.toString());
 
+		}
 	}
 }
--- Practice/src/practice18/PTra18_04.java
+++ Practice/src/practice18/PTra18_04.java
@@ -6,6 +6,14 @@
  */
 package practice18;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+import entity.Player;
+
 public class PTra18_04 {
 
 	/*
@@ -19,9 +27,70 @@ public class PTra18_04 {
 		 * ★ file/BestElevenCandidate.csvの内容を取得し、１行毎にPlayerインスタンスに情報を格納してください
 		 * ★ ArrayListを作成して、Playerインスタンスを格納してください
 		 */
+		ArrayList<Player> array = new ArrayList<Player>();
+		try (Scanner scanner = new Scanner(new File("file/BestElevenCandidate.csv"))) {
+			while (scanner.hasNext()) {
+				String line = scanner.nextLine();
+				String[] array1 = line.split(",", 0);
+				Player player = new Player();
 
+				player.setPosition(array1[0]);
+				player.setName(array1[1]);
+				player.setCountry(array1[2]);
+				player.setTeam(array1[3]);
+
+				array.add(player);
+
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("ファイルが見つかりません");
+		}
 
 		// ★ ①のArrayListの中からGK1名、DF4名、MF4名, FW2名をランダムで出力してください
+		Collections.shuffle(array);
+		for (int i = 0; i < array.size(); i++) {
+			if (array.get(i).getPosition().contains("GK")) {
+				System.out.println(array.get(i));
 
+				break;
+			}
+		}
+
+		int count = 0;
+		for (int i = 0; i < array.size(); i++) {
+			if (array.get(i).getPosition().contains("DF")) {
+				System.out.println(array.get(i));
+				count++;
+
+				if (count >= 4) {
+					break;
+				}
+			}
+
+		}
+		count = 0;
+		for (int i = 0; i < array.size(); i++) {
+			if (array.get(i).getPosition().contains("MF")) {
+				System.out.println(array.get(i));
+
+				count++;
+
+				if (count >= 4) {
+					break;
+				}
+			}
+
+		}
+		count = 0;
+		for (int i = 0; i < array.size(); i++) {
+			if (array.get(i).getPosition().contains("FW")) {
+				System.out.println(array.get(i));
+				count++;
+
+				if (count >= 2) {
+					break;
+				}
+			}
+		}
 	}
 }
